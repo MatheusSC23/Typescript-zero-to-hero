4059d88fb5bb625411920e1a2d0bb9ee143b6dbc
MatheusOSF <matheus.sousa@osf.digital>
2023-05-22 15:14:40 -0300

    estudo do tipo primitivo string

---
+++ Modulo2/aula14/stringExample.js
@@ -0,0 +1,8 @@
+"use strict";
+let nomeCompleto = 'Matheus Sousa Correia';
+console.log(nomeCompleto);
+let funcaoEmpresa = "Frontend Developer";
+console.log(funcaoEmpresa);
+let nomeEmpresa = "OSF";
+let dadosFuncionario = `Nome: ${nomeCompleto} | Função: ${funcaoEmpresa} | Empresa: ${nomeEmpresa}`;
+console.log(dadosFuncionario);
---
+++ Modulo2/aula14/stringExample.ts
@@ -0,0 +1,10 @@
+let nomeCompleto: string = 'Matheus Sousa Correia';
+console.log(nomeCompleto)
+
+let funcaoEmpresa: string = "Frontend Developer";
+console.log(funcaoEmpresa)
+
+let nomeEmpresa: string = "OSF";
+
+let dadosFuncionario: string = `Nome: ${nomeCompleto} | Função: ${funcaoEmpresa} | Empresa: ${nomeEmpresa}`;
+console.log(dadosFuncionario)
